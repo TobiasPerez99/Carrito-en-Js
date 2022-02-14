55882671fda4100e8ea5ebc46d06a39b8afa99bc
Tobias Perez <tobiasleonel77@gmail.com>
2022-02-14 02:20:05 -0300

    manejando cantidades

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //variable
 const carrito = document.querySelector('#carrito');
 const listarCursos = document.querySelector('#lista-cursos');
-const contenedorCarrito = document.querySelector('#listar-carrito tbody');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 let articulosCarrito = []
 
@@ -24,7 +24,7 @@ function agregarCurso(e) {
 }
 
 function leerDatoCurso(curso) {
-    console.log(curso);
+    // console.log(curso);
 
     const infoCurso = {
         imagen: curso.querySelector('img').src,
@@ -34,7 +34,65 @@ function leerDatoCurso(curso) {
         cantidad: 1
     }
 
-    // agregar elemeentos al arreglo de carrito
 
-    articulosCarrito
+
+    const existe = articulosCarrito.some(curso => curso.id === infoCurso.id);
+
+    if (existe) {
+        const cursos = articulosCarrito.map(curso => {
+            if (curso.id === infoCurso.id) {
+                curso.cantidad++;
+                return curso;
+            } else {
+                return curso;
+            }
+        })
+
+        articulosCarrito = [...cursos];
+
+    } else {
+
+        // agregar elemeentos al arreglo de carrito
+
+        articulosCarrito = [...articulosCarrito, infoCurso];
+    }
+
+    carritoHTML();
+}
+
+
+//muestra el carrito de compra
+function carritoHTML() {
+
+    vaciarCarrito();
+
+    console.log(contenedorCarrito);
+    console.log(articulosCarrito);
+
+    articulosCarrito.forEach(curso => {
+        const row = document.createElement('tr');
+        row.innerHTML = `<img src = "${curso.imagen}" widht="100" alt="imagencurso">         
+         <td> ${curso.nombre}</td>
+         <td> ${curso.precio}</td>
+         <td> ${curso.cantidad}</td>
+         <td> <a href="#" class = "borrar-curso" data-id="${curso.id}">X</a></td>`;
+
+        contenedorCarrito.appendChild(row);
+    });
+
+}
+// Muestra el curso seleccionado en el Carrito
+
+
+
+// Elimina los cursos del carrito en el DOM
+function vaciarCarrito() {
+    // forma lenta
+    // contenedorCarrito.innerHTML = '';
+
+
+    // forma rapida (recomendada)
+    while (contenedorCarrito.firstChild) {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
 }
